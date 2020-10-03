ed884010e1c7506e266f42a2a4047508a0cc7573
iamsomraj <iamsomraj@gmail.com>
2020-10-03 20:19:33 +0530

    Added LCS and fixed Freq Of Chars in Java programs

--- Java programs/README.md
+++ Java programs/README.md
@@ -2,3 +2,4 @@
 
 - [Frequency of characters](frequency_of_char.java)
 - [Longest Common Subsequence](longest_common_subsequence.java)
+- [Longest Pallindromic Subsequence](longest_pallindromic_subsequence.java)
--- Java programs/frequency_of_char.java
+++ Java programs/frequency_of_char.java
@@ -1,49 +1,22 @@
-package virtusa_practise;
-
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
-public class Duplicates {
+public class frequency_of_char {
 
 	public static void main(String[] arg) {
-		String x="abcabcd";
-		char arr[]=x.toCharArray();
-		Map<Character,Integer> map=new HashMap();
-		int f[]=new int[arr.length];
-		for(int i=0;i<arr.length;i++)
-		{
-//			f[i]=1;
-//			for(int j=i+1;j<arr.length;j++)
-//			{
-//				if(arr[i]==arr[j])
-//				{
-//					f[i]++;
-//					arr[j]='0';
-					if(map.containsKey(arr[i]))
-					    map.put(arr[i],map.get(arr[i])+1);
-					else
-						 map.put(arr[i],1);
-//				}
-//				else
-//				{
-//					
-//				}
-//			}
+		String x = "abcabcd";
+		char arr[] = x.toCharArray();
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if (map.containsKey(arr[i]))
+				map.put(arr[i], map.get(arr[i]) + 1);
+			else
+				map.put(arr[i], 1);
 		}
-		
-		Set<Character> set=map.keySet();
-		
-		for(char c:set)
-		{
-			System.out.println("frequency of "+c+" is "+map.get(c));
+		Set<Character> set = map.keySet();
+		for (char c : set) {
+			System.out.println("Frequency of " + c + " is " + map.get(c));
 		}
-//		for(int i=0;i<arr.length;i++)
-//		{
-//			if(arr[i]!='0')
-//			{
-//				System.out.println("value is: "+arr[i]+"frequency is : "+f[i]);
-//			}
-//		}
+
 	}
 }
