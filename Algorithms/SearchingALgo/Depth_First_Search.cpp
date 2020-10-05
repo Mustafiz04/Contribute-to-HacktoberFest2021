9ba5994b88afc82dbbf0d95ab7272d67e0ab8f02
Surajbokde <66197682+Surajbokde@users.noreply.github.com>
2020-10-05 18:06:48 +0530

    Depth First Search
    Depth First Search is one of the main graph algorithms.
    
    Depth First Search finds the lexicographical first path in the graph from a source vertex u to each vertex. Depth First Search will also find the shortest paths in a tree (because there only exists one simple path), but on general graphs this is not the case.
    
    The algorithm works in O(m+n) time where n is the number of vertices and m is the number of edges.

---
+++ Algorithms/SearchingALgo/Depth_First_Search.cpp
@@ -0,0 +1,14 @@
+//Depth First Search algorithm in cpp
+
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; // number of vertices
+
+vector<bool> visited;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj[v]) {
+        if (!visited[u])
+            dfs(u);
+    }
+}
