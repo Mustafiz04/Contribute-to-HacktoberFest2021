b67360a04223b9e79405a5496260fe88c0d0389f
ksgsarma <33192739+ksgsarma@users.noreply.github.com>
2020-10-18 22:51:36 +0530

    Added Java Project

---
+++ Projects/PhoneAuthentication/README.md
@@ -0,0 +1,3 @@
+# Phone-Authentication
+## -By: KSGSARMA
+This is the code for creating an android application which send an OTP to the mobile number when given and validates it.
---
+++ Projects/PhoneAuthentication/verifyPhone.java
@@ -0,0 +1,150 @@
+package com.parveen.example.phoneauth;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class verifyPhone extends AppCompatActivity {
+
+    //These are the objects needed
+    //It is the verification id that will be sent to the user
+    private String mVerificationId;
+
+    //The edittext to input the code
+    private EditText editTextCode;
+
+    //firebase auth object
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+
+        //initializing objects
+        mAuth = FirebaseAuth.getInstance();
+        editTextCode = findViewById(R.id.editText);
+
+
+        //getting mobile number from the previous activity
+        //and sending the verification code to the number
+        Intent intent = getIntent();
+        String mobile = intent.getStringExtra("mobile");
+        sendVerificationCode(mobile);
+
+
+        //if the automatic sms detection did not work, user can also enter the code manually
+        //so adding a click listener to the button
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = editTextCode.getText().toString().trim();
+                if (code.isEmpty() || code.length() < 6) {
+                    editTextCode.setError("Enter valid code");
+                    editTextCode.requestFocus();
+                    return;
+                }
+
+                //verifying the code entered manually
+                verifyVerificationCode(code);
+            }
+        });
+
+    }
+
+    //the method is sending verification code
+    //the country id is concatenated
+    //you can take the country id as user input as well
+    private void sendVerificationCode(String mobile) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + mobile,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    }
+
+
+    //the callback to detect the verification status
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            //Getting the code sent by SMS
+            String code = phoneAuthCredential.getSmsCode();
+
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+            if (code != null) {
+                editTextCode.setText(code);
+                //verifying the code
+                verifyVerificationCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(verifyPhone.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            //storing the verification id that is sent to the user
+            mVerificationId = s;
+        }
+    };
+
+
+    private void verifyVerificationCode(String code) {
+        //creating the credential
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        //signing the user
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(verifyPhone.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //verification successful we will start the profile activity
+                            Intent intent = new Intent(verifyPhone.this, Welcome.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+                        } else {
+
+                            //verification unsuccessful.. display an error message
+                            editTextCode.setError("Please Enter valid OTP!");
+
+                        }
+                    }
+                });
+    }
+
+}
+
