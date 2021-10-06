b214e7e948f26d32e30229357806891a26d674e8
Heshani Nimalka <54610745+Heshani29@users.noreply.github.com>
2021-10-06 15:11:24 +0530

    Update HW.cs

--- Hello World Programs/HW.cs
+++ Hello World Programs/HW.cs
@@ -1,8 +1,10 @@
-// Your First C++ Program
-
-#include <iostream>
-
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+// Hello World! program
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
 }
