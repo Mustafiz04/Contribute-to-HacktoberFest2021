dbec092bf42fbb20b387d36afeb42505361e148e
Vashishth Patel <76911582+vasu-1@users.noreply.github.com>
2021-10-13 10:34:54 +0530

    added remaining files

---
+++ Projects/Web-Chat-App/post.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION['name'])){
+    $text = $_POST['text'];
+     
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+}
+?>
